a11d1eb3747f1fd82d6a5c73b719b836690ea825
monoidor <o-radik@ya.ru>
2022-04-19 19:22:50 +1000

    Убирает вывод в консоль полученные с сервера данные

--- js/main.js
+++ js/main.js
@@ -1,8 +1,2 @@
 import './thumbnail.js';
 import './upload-form.js';
-
-fetch('https://25.javascript.pages.academy/kekstagram/data')
-  .then((response) => response.json())
-  .then((photos) => {
-    console.log(photos);
-  });
